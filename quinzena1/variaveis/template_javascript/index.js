7f0fb58d5b180eaef698d4a6718e8ed730e74818
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2021-09-08 21:29:18 -0300

    Hoje foi um desastre

---
+++ quinzena1/variaveis/template_javascript/index.js
@@ -0,0 +1,7 @@
+
+let nomeUsuario =prompt('Digite o nome de usuário')
+let idadeUsuario =prompt('Digite sua idade')
+
+console.log(nomeUsuario, idadeUsuario)
+
+console.log( "Olá" nomeUsuario , "você tem" idadeUsuario "anos")
