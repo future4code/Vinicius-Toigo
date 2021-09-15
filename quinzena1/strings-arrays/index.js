bef0a3dc0b45b23b7e734b9cb979e7ee362b459e
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2021-09-14 21:35:41 -0300

    strings-arrays

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,90 @@
+//                                             Exercícios de interpretação de código
+
+
+
+//1 
+
+//a. undefined
+//b. null
+//c. 11
+//d. 3
+//e. 
+//f. 9
+
+
+//2
+
+//SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+
+
+
+
+//                                               Exercícios de escrita de código
+
+
+//                                              Exercício 1  
+
+//let seuNome =prompt( "Digite seu nome.")
+//let seuEmail =prompt( "Digite seu email")
+
+//let frase = "O email" + seuEmail + "foi cadastrado com sucesso. Seja bem-vindo(a)," + seuNome
+
+
+//console.log(seuNome , seuEmail )
+
+//alert("O email"  + seuEmail + "foi cadastrado com sucesso. Seja bem-vindo(a)," + seuNome) 
+
+
+
+//                                               Exercício 2
+
+
+
+    //Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável. Em seguida, siga os passos:
+
+    // a) Imprima na tela o array completo
+
+    //b) Imprima na tela a mensagem "Essas são as minhas comidas preferidas: ", seguida por cada uma das comidas, **uma embaixo da outra**.
+
+    //c) Aqui vai um desafio: pergunte ao usuário uma comida preferida. Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima na tela a nova lista
+
+    //const comidasPreferidas = [ "Essas são as minhas comidas preferidas",
+    // "Pizza", 
+     //"Massa",
+    // "Strogonoff",
+    // "Hambúrguer",
+    //  "Xis"]
+
+   // alert(comidasPreferidas)
+
+   // console.log (comidasPreferidas) 
+
+    //const aPergunta =prompt( "Digite qual é a sua comida preferida.")
+
+    //const troca = comidasPreferidasreplace.all (2 , aPergunta)
+
+
+
+    //                                   Exercício 3 
+
+
+
+//a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`
+//b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+//c) Imprima o array na tela
+//d) Peça ao usuário que digite o **índice** de uma tarefa que ele já realizou: 0, 1 ou 2 
+//e) Remova da lista o item de índice que o usuário escolheu.
+//f) Imprima o array na tela
+  
+//let listaDeTarefas= []
+
+//let asTarefas =prompt( "Quais seriam as 3 tarefas que você precisa realizar hoje? Digite todas neste campo.")
+
+//console.log (asTarefas) 
+
+
+//let mudarValores = asTarefas
+
+
+//alert(listaDeTarefas) 
