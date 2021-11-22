1e300554ffe1a3570eeb20d1b57cea80b9b00132
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2021-11-22 18:50:18 -0300

    projeto-whatslab

---
+++ quinzena5/projeto-whatslab/src/styles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const listaDeComponentes = styled.p`
+    align-items:center;
+  padding: 100px;
+  background-color: white;
+  border: none;
+  border-radius: 55px;
+  box-shadow: 33px 33px 6px 0px rgba(0, 0, 0, 0.85); 
+  margin-right: 10px;
+ 
+  `;
