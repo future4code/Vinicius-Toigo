18071d897d9270424a1191ed53ba8613ddb7da78
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2022-03-17 20:37:44 -0300

    aula-node

---
+++ modulo6/Exercício2/index.js
@@ -0,0 +1,36 @@
+// - **Exercício 2**
+    
+//     Crie uma aplicação Node que recebe uma string representando uma operação
+//     matemática e dois valores numéricos. O retorno deverá ser o 
+//     resultado da operação selecionada utilizando os 2 valores fornecidos.
+
+
+
+// const operation=process.argv[2]
+// const num1=Number(process.argv[3])
+// const num2=Number(process.argv[4])
+
+// switch (operation) {
+	
+//     case 'add':
+// 		const result=num1+num2
+// 	console.log(`Resposta: ${result}`);
+// 		break;
+	
+//         case 'sub':
+// 	const	resultSub=num1-num2
+// 	console.log(`Resposta: ${resultSub}`);
+// 		break;
+	
+//         case 'mult':
+// 	const	resultMult=num1*num2
+// 	console.log(`Resposta: ${resultMult}`);
+// 		break;
+	
+//         case 'div':
+// 		const resultDiv=num1/num2
+// 	console.log(`Resposta: ${resultDiv}`);
+// 		break
+// 	default:
+// 		break;
+// } 
---
+++ modulo6/Exercício3/index.js
@@ -0,0 +1,16 @@
+
+// - **Exercício 3**
+    
+// Crie uma aplicação Node que receba uma string representando 
+// uma tarefa. O programa deve adicionar a nova tarefa
+// em uma variável que represente uma lista de tarefas.
+// A lista de tarefas pode estar criada antes da execução do código. 
+// Após adicionar o item à lista, exiba a lista atualizada.
+
+
+// const tarefas = process.argv[2]
+// const listaTarefas = ["Estudar","Descansar","Jogar"]
+
+// const novaListaTarefas = listaTarefas.push(tarefas)
+
+// console.log(listaTarefas)
