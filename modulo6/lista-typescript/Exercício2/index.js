8725429907105cc8fea78302413ff97264dac7cb
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2022-03-22 21:02:48 -0300

    ...

---
+++ modulo6/lista-typescript/Exercício2/index.js
@@ -0,0 +1,9 @@
+// Exercício 2 
+
+// Crie uma função que receba um parâmetro qualquer e 
+// que imprima no console o tipo da variável.
+
+const nome = process.argv[2]
+const idade = process.argv[3]
+
+console.log(`Olá, ${nome} Você tem ${idade} anos`)
